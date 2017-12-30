1a819b5549e33d605cbaa5dc74e7c89ab437c8c5
Kyle Ruschel <dkr1985@icloud.com>
2017-12-30 00:15:49 -0500

    updates

--- public/app.js
+++ public/app.js
@@ -1,19 +1,32 @@
 // Grab the articles as a json
 $("#butt").on("click", function() {
     $.getJSON("/all", function(data) {
-        var title = [];
-        var link = [];
-        var excerpt = [];
-            for(var i = 0; i < data.length; i++) {
-                title.push(data[i].title);
-                link.push(data[i].link);
-                excerpt.push(data[i].excerpt);
-            }
-            console.log(title);
-            console.log(link);
-            console.log(excerpt);
+        //console.log(data);
+        let lines = data;
+        let output = '';
+        $.each(data, function(i, element) {
+            output += `
+                        <div class=row>
+                            <div class="col-md-12">
+                                <div class="text-center">
+                                    <h4 class="title">'${element.title}</h4>
+                                    \n
+                                    <img src="${element.photo}" alt="photo">
+                                    <p>'${element.excerpt}'</p>
+                                    <a href="${element.link}">'${element.link}'</a>
+                                </div>
+                            </div>
+                        </div>`
+        })
+
+        $('#info').html(output);
+
         });
+
     });
+
+
+
 
 
 
